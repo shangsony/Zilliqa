c45ac4d9a36c78ea4d1d25824579babd1036204f
bb111189 <junhao@zilliqa.com>
2018-07-26 14:03:12 +0800

    Merging fix

--- src/libDirectoryService/DirectoryService.h
+++ src/libDirectoryService/DirectoryService.h
@@ -291,6 +291,7 @@ class DirectoryService : public Executable, public Broadcastable
                                     unsigned int offset, const Peer& from);
 
     // View change
+    void SetLastKnownGoodState();
     void RunConsensusOnViewChange();
     void ScheduleViewChangeTimeout();
     void ComputeNewCandidateLeader();
